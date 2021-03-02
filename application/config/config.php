792d907f70dea7410420daff2a8c5fd9e394606b
SOUMYA993275 <soumya993275@gmail.com>
2021-03-02 19:12:08 +0530

    .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/snrd/';
+$config['base_url'] = 'https://xpanel.sangbadraatdin.in/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +35,7 @@ $config['base_url'] = 'http://localhost/snrd/';
 | variable so that it is blank.
 |
 */
+//$config['index_page'] = 'index.php';
 $config['index_page'] = '';
 
 /*
